4088219156ccb96ce3c9e3834c5cf627fb1ce05a
Sean Oyler <xrysen@gmail.com>
2021-03-07 21:06:17 -0800

    Adds Diagnostics

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace DotNetDebugging
 {
@@ -11,6 +12,8 @@ namespace DotNetDebugging
         }
         static int Fibonacci(int n)
         {
+            Debug.WriteLine($"Entering {nameof(Fibonacci)} method");
+            Debug.WriteLine($"We are looking for the {n}th number");
             int n1 = 0;
             int n2 = 1;
             int sum = 0;
@@ -20,7 +23,9 @@ namespace DotNetDebugging
                 sum = n1 + n2;
                 n1 = n2;
                 n2 = sum;
+                Debug.WriteLineIf(sum == 1, $"sum is 1, n1 is {n1}, n2 is {n2}");
             }
+            Debug.Assert(n2 == 5, "The return value is not 5 and it should be.");
             return n == 0 ? n1 : n2;
         }
     }
